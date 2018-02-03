ed59be03bc6ac1ad625b574cdb0f3c2d32271b48
Evyatar Meged <cambina1@gmail.com>
2018-02-03 19:43:35 +0200

    proj init

---
+++ js/script.js
@@ -0,0 +1,2 @@
+// Scraping, parsing & notifying logic goes here. Using jQuery AJAX requests (no CORS limitation). yay
+
---
+++ main.js
@@ -0,0 +1,65 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+
+function runGUI() {
+
+}
+
+// 4pr0z
+function runHeadless(args) {
+	let queryInterval = args[2]
+	let tags = args.slice(3)
+	mainWindow = new BrowserWindow({width: 800, height: 600})
+
+	// and load the index.html of the app.
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file:',
+		slashes: true
+	}))
+
+	mainWindow.on('closed', () => {
+		mainWindow = null
+	})
+}
+
+
+function createWindow() {
+	let args = process.argv
+	if (args.length <= 2) {
+		runGUI(args)
+	} else {
+		runHeadless(args)
+	}
+
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+})
+
+app.on('activate', () => {
+	// On macOS it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		createWindow()
+	}
+})
