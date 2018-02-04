b8260271c8911ed3791ae8a96ac0b02872dac3ae
Evyatar Meged <cambina1@gmail.com>
2018-02-04 22:43:21 +0200

    progress

--- js/argVal.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-// Export function that validates params
-
-
-
--- js/headless.js
+++ js/headless.js
@@ -1,1 +1,39 @@
 // Scraping, parsing & notifying logic goes here. Using jQuery AJAX requests (no CORS limitation). yay
+
+$(function() {
+
+	const BASE_URL = 'https://stackoverflow.com/questions/tagged/',
+		SUFFIX = '?sort=newest&pageSize=15',
+		TAGS = $('#tags').text().split(' ').join('+'),
+		qryInterval = $('#query-interval').text(),
+		URL = BASE_URL + TAGS + SUFFIX;
+
+	let newQuestions = new Set();
+
+	function getNotifications() {
+		// (h3 > a).text() = question title
+		// (.user-details > a).text() = asker
+		// (.user-action-time > span).getAttribute(title) = timestamp
+		// OMMFG PROGRESS
+
+		$.ajax({
+			type: 'GET',
+			url: URL,
+			success: (page) => {
+				console.log()
+				let questions = $(page).find('.question-summary');
+				$.each(questions, (index, item) => {
+					console.log($(item).find('h3 > a').text());
+				})
+			},
+			error: (err) => {
+				console.log(err)
+			}
+		})
+	}
+
+	getNotifications()
+
+
+
+})
