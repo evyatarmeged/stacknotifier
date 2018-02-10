3111c1c8aea86ca2f2bdd41ea7bb5f4dd131ffdd
Evyatar Meged <cambina1@gmail.com>
2018-02-10 21:27:06 +0200

    refactoring

--- js/inquirer.js
+++ js/inquirer.js
@@ -1,39 +1,67 @@
-// bzz
+'use strict';
+
+const path = require('path'),
+	notifier = require(path.join(__dirname, 'js/notifier.js')),
+	baseUrl = 'https://stackoverflow.com/',
+	suffix = '?sort=newest&pageSize=15',
+	tagString = 'questions/tagged/' + $('#tags').text().replace(/,/g, '+').toLowerCase(),
+	qryInterval = $('#query-interval').text(),
+	completeUrl = baseUrl + tagString + suffix;
+
+let queue = [];
 
 
+const sortByTimeStamp = (a,b) => {
+	if (a.ts < b.ts)
+		return 1;
+	if (a.ts > b.ts)
+		return -1;
+	return 0;
+}
+
+const addToQueue = item => {
+	// Enforce max size on queue
+	if (queue.length === 15) {
+		queue.pop()
+	}
+	queue.unshift(item)
+}
+
+
+const getDateTimeFromTimestamp = unixTimeStamp => {
+	let date = new Date(unixTimeStamp);
+	return ('0' + date.getDate()).slice(-2) +
+		'/' + ('0' + (date.getMonth() + 1)).slice(-2) +
+		'/' + date.getFullYear() + ' ' +
+		('0' + date.getHours()).slice(-2) + ':' +
+		('0' + date.getMinutes()).slice(-2);
+}
+
+const isQuestionExists = questionObj => {
+	let exists = false,
+		index = 0;
+	while (!exists && index < queue.length) {
+		let current = queue[index];
+		exists = compare(questionObj, current)
+		index ++;
+	}
+	return exists;
+}
+
+const compare = (a, b) => {
+	return a.title === b.title && a.ts === b.ts
+}
+
+
+// Flow
 $(function() {
 
-	const path = require('path'),
-		notifier = require(path.join(__dirname, 'js/notifier.js')),
-		baseUrl = 'https://stackoverflow.com/',
-		suffix = '?sort=newest&pageSize=15',
-		tags = 'questions/tagged/' + $('#tags').text().replace(/,/g, '+').toLowerCase(),
-		qryInterval = $('#query-interval').text(),
-		completeUrl = baseUrl + tags + suffix;
-
-
-	let queue = [];
-
-	const sortByTimeStamp = (a,b) => {
-		if (a.ts < b.ts)
-			return 1;
-		if (a.ts > b.ts)
-			return -1;
-		return 0;
-	}
-
-	const addToQueue = item => {
-		// Enforce max size on queue
-		if (queue.length === 15) {
-			queue.pop()
-		}
-		queue.unshift(item)
-	}
 
 	async function parseQuestions(page) {
 		let $page = $(page),
 			questions = $page.find('.question-summary'),
-			newQuestions = [];
+			newQuestions = [],
+			newQuesCount = 0;
 
 		$.each(questions, (index, item) => {
 			let $item = $(item),
@@ -51,52 +79,32 @@ $(function() {
 
 			if (queue.length !== 15) {
 				queue.push(questionObj)
-			} else if (!isQuestionExists(questionObj)) {
-				newQuestions.push(questionObj)
+			} else {
+				newQuestions.push(questionObj);
+				if (!isQuestionExists(questionObj)) {
+					newQuesCount ++;
+				}
 			}
 		})
 
-		queue.sort(sortByTimeStamp)
+		// TODO: Fix questions not being in the right order (newest -> oldest) after 1st run
+		// queue.sort(sortByTimeStamp)
 
-		if (newQuestions.length !== 0) {
-			newQuestions.reverse().forEach((obj) => {
-				addToQueue(obj)
-			})
+		// if (newQuestions.length !== 0) {
+		// 	newQuestions.reverse().forEach((obj) => {
+		// 		addToQueue(obj)
+		// 	})
 
-			queue.forEach((item) => {
-				console.log(getDateTimeFromTimestamp(item.ts))
-			})
+			// queue.forEach((item) => {
+			// 	console.log(getDateTimeFromTimestamp(item.ts))
+			// })
+		// }
+		if (newQuestions === 15) {
+			queue = newQuestions;
 		}
-
-		return newQuestions;
+		console.log(newQuesCount);
+		return newQuesCount;
 	}
-
-
-	const getDateTimeFromTimestamp = (unixTimeStamp) => {
-		let date = new Date(unixTimeStamp);
-		return ('0' + date.getDate()).slice(-2) +
-			'/' + ('0' + (date.getMonth() + 1)).slice(-2) +
-			'/' + date.getFullYear() + ' ' +
-			('0' + date.getHours()).slice(-2) + ':' +
-			('0' + date.getMinutes()).slice(-2);
-	}
-
-	const isQuestionExists = questionObj => {
-		let exists = false,
-			index = 0;
-		while (!exists && index < queue.length) {
-			let current = queue[index];
-			exists = compare(questionObj, current)
-			index ++;
-		}
-		return exists;
-	}
-
-	const compare = (a, b) => {
-		return a.title === b.title && a.ts === b.ts
-	}
-
-
 
 	const getQuestionPage = () => {
 		// Life is good without CORS
@@ -106,8 +114,8 @@ $(function() {
 			url: completeUrl,
 			success: page => {
 				parseQuestions(page)
-					.then((newQuestions) => {
-						notifier.notify(newQuestions)
+					.then((newQuesCount) => {
+						notifier.notify(newQuesCount, queue)
 					})
 					.catch((err) => {
 						console.error(err)
--- js/notifier.js
+++ js/notifier.js
@@ -1,17 +1,18 @@
 
+const open = require('open');
+
 module.exports = {
 
-	notify: function(quesArr, tags) {
-		if (quesArr !== undefined && quesArr.length !== 0) {
-
+	notify: function(newQuesCount, quesArr) {
+		if (newQuesCount > 0) {
 			let notif = new Notification('New Questions. Click to view', {
-				body: quesArr[0].title + '\r\n' + 'Asked by: ' + quesArr[0].asker,
+				body: quesArr[0].title + '\r\n\r\n' + 'Asked by: ' + quesArr[0].asker,
 				icon: 'sof.png'
 			})
 
 			notif.onclick = function(event) {
 				event.preventDefault();
-				window.open(quesArr[0].url, '_blank');
+				open(quesArr[0].url);
 			}
 		}
 	}
