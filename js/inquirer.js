68f4efdf9c3ff5840b5bf846b86ca3c106514e90
Evyatar Meged <cambina1@gmail.com>
2018-02-18 20:51:52 +0200

    making progress. or am i ?

--- js/inquirer.js
+++ js/inquirer.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 const path = require('path'),
 	Notifier = require(path.join(__dirname, 'js/notifier.js')),
@@ -6,37 +6,28 @@ const path = require('path'),
 	suffix = '?sort=newest&pageSize=15',
 	tagString = 'questions/tagged/' + $('#tags').text().replace(/,/g, '+').toLowerCase(),
 	qryInterval = $('#query-interval').text(),
-	completeUrl = baseUrl + tagString + suffix;
-
-let queue = [];
+	completeUrl = baseUrl + tagString + suffix
 
 
 const sortByTimeStamp = (a,b) => {
-	if (a.ts < b.ts)
-		return 1;
-	if (a.ts > b.ts)
-		return -1;
-	return 0;
-};
-
-
-const addToQueue = element => {
-	// Enforce max size on queue
-	if (queue.length === 15) {
-		queue.pop()
+	if (a.ts < b.ts) {
+		return 1
 	}
-	queue.unshift(element)
-};
+	if (a.ts > b.ts) {
+		return -1
+	}
+	return 0
+}
 
 
 const isQuestionExists = (arr, questionObj) => {
-	let result = arr.find(element => element.ts === questionObj.ts);
-	return result !== undefined && result.title === questionObj.title;
-};
+	let result = arr.find(element => element.ts === questionObj.ts)
+	return result !== undefined && result.title === questionObj.title
+}
 
 
 const parseQuestionToObject = item => {
-	let $item = $(item);
+	let $item = $(item)
 	return {
 		title: $item.find('h3 > a').text(),
 		body: $item.find('.excerpt').text().trim(),
@@ -44,35 +35,43 @@ const parseQuestionToObject = item => {
 		url: baseUrl + $item.find('.question-hyperlink').attr('href'),
 		ts: Date.parse($item.find('.user-action-time > span').attr('title'))
 	}
-};
+}
+
 
 // Flow
 $(function() {
 
-	let notifier = new Notifier()
+	let notifier = new Notifier(),
+		queue = []
+
 
 	function getNewBatch(page) {
 		return new Promise((resolve, reject) => {
 			let $page = $(page),
 				questions = $page.find('.question-summary'),
-				newQuestionsCount = 0;
+				newQuestionsCount = 0
 
 			$.each(questions, (_, item) => {
-				let questionObj = parseQuestionToObject(item);
+				let questionObj = parseQuestionToObject(item)
 				// First run to collect base case data to compare against
 				if (queue.length !== 15) {
 					queue.push(questionObj)
-				} else if (isQuestionExists(queue, questionObj)) {
-					// Replace/fix addToQueue logic - it's incorrect
-					addToQueue(questionObj);
-					newQuestionsCount++;
+				} else if (!isQuestionExists(queue, questionObj)) {
+
+					// Still lacking. Needs improvement.
+
+					queue.unshift(questionObj);
+					queue.pop();
+					newQuestionsCount++
 				}
-			});
-			queue.sort(sortByTimeStamp);
+			})
+
+			queue.sort(sortByTimeStamp)
 			// TODO: Test new isQuestionExist to see if accurate ts' are being used
-			resolve(newQuestionsCount);
+			resolve(newQuestionsCount)
 		})
 	}
+
 
 	const getQuestionPage = () => {
 		// Life is good without CORS
@@ -82,10 +81,11 @@ $(function() {
 			url: completeUrl,
 			success: page => {
 				getNewBatch(page)
-					.then(() => {
-						// notifier.genericNotify(5, queue)
+					.then((result) => {
+						// ### Notify all new questions ###
+						console.log(result)
 						queue.forEach((item) => {
-							console.log(item)
+							console.log(notifier.getDateTimeFromTimestamp(item.ts))
 						})
 					})
 					.catch((err) => {
@@ -96,16 +96,14 @@ $(function() {
 				console.log(err)
 			}
 		})
-	};
+	}
 
-	const callAll = () => {
-		// Urgent: find a better name than "callAll" :<
-		getQuestionPage();
+	const execute = () => {
+		getQuestionPage()
 		setTimeout(() => {
-			callAll()
-		}, 60000)};
+			execute()
+		}, 120000)}
 
+	execute()
 
-	callAll()
-
-});
+})
